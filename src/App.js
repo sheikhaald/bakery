d51674e373f9b79d0c85ca9c7b2c042009dd96df
sheikhaald <smaldeai98@gmail.com>
2023-10-01 19:04:17 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="logo">
+      <h1>MY Pink Bakery</h1>
+      <p>bake with love</p>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://s-media-cache-ak0.pinimg.com/736x/d5/e6/12/d5e612b085c50ac2c0034ff14d90e01f--door-county-wisconsin-pink-stuff.jpg"
+          className="App-logo"
+          alt="logo"
+        />
       </header>
     </div>
   );
